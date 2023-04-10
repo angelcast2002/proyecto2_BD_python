d6f4ef36d7e47c3088b27af5090c8b6a537964c3
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2023-04-10 09:38:59 -0600

    avances

--- expediente.py
+++ expediente.py
@@ -1,7 +1,10 @@
 from tkinter import *
+import connection as con
+from resultadosExpediente import ResultadoExpediente
+from signup import SignupWindow
 
 class ExpedienteWindow:
-    def __init__(self, parent):
+    def __init__(self, parent, administrador):
         self.parent = parent
         self.win = Toplevel(parent)
         self.win.title("Expediente")
@@ -12,20 +15,30 @@ class ExpedienteWindow:
         inputNombrePaciente = Entry(self.win)
         inputCodigoPaciente = Entry(self.win)
         
-        buttonBuscar = Button(self.win, text="Buscar", command= lambda: self.buscarPaciente(inputNombrePaciente, inputCodigoPaciente))
+        buttonBuscar = Button(self.win, text="Buscar", command= lambda: self.buscarPaciente(inputIdPaciente))
+        buttonCrearUsuario = Button(self.win, text="Crear usuario", command= lambda: self.crearUsuario())
+        buttonEditarUsuario = Button(self.win, text="Editar usuario", command= lambda: self.close())
         
         etiNombrePaciente.pack()
         inputNombrePaciente.pack()
         etiCodigopaciente.pack()
         inputCodigoPaciente.pack()
         buttonBuscar.pack()
-        
+        if (administrador == True):
+            buttonCrearUsuario.pack()
+            buttonEditarUsuario.pack()
         
         self.win.geometry("300x200")
         
-    def buscarPaciente(self,inputNombrePaciente, inputCodigoPaciente):
-        etiNombreResultado = Label(self.win, text=inputNombrePaciente.get())
-        etiCodigoResultado = Label(self.win, text=inputCodigoPaciente.get())
+    def buscarPaciente(self, inputIdPaciente):
+        query = f"SELECT * FROM paciente p WHERE p.dpi = '{inputIdPaciente.get()}'"
+        results = con.connect(query)
+        column_names = con.column_names(query)
+        ResultadoExpediente(self.win, results, column_names)
+        
+    def crearUsuario(self):
+        SignupWindow(self.win)
+    
         
         etiNombreResultado.pack()
         etiCodigoResultado.pack()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from signup import SignupWindow
+from tkinter import messagebox
 from expediente import ExpedienteWindow
 import connection as con
 
@@ -22,26 +22,30 @@ def principalWindow():
     inputPassword = Entry(win)
     
     buttonLogin = Button(win, text="Login", command= lambda: login(inputPassword, inputUsername, win))
-    buttonSignup = Button(win, text="Signup", command= lambda: signUp(win))
     
     etiUsername.pack()
     inputUsername.pack()
     etiPassword.pack()
     inputPassword.pack()
     buttonLogin.pack()
-    buttonSignup.pack()
 
     win.geometry("300x200")
     win.mainloop()
     
-def signUp(win):
-    SignupWindow(win)
+# Login function
+def login(inputUsername, inputPassword, win):
+    usuario = inputUsername.get()
+    contrasena = inputPassword.get()
+    query = f"SELECT COUNT(*) FROM usuarios WHERE usuario='{usuario}' AND contrasena='{contrasena}'" # Query
+    results = con.connect(query)
     
-def login(inputPassword, inputUsername, win):
-    if (inputPassword.get() == "123" and inputUsername.get() == "123"):
-        ExpedienteWindow(win)
+    # None type data verification
+    if results is not None and results[0][0] == 1:
+        ExpedienteWindow(win, administrador=True)
+        
+    # Error message if credentials do not match
+    else:
+        messagebox.showerror("Error de inicio de sesión", "Usuario o contraseña incorrectos")
 
-
-    
 if __name__ == '__main__':
     main()
